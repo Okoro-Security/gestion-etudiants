dc463434c9d9453b7844b5f9ab832f0ce9c206b1
Lom Emeka Collins <lomemeka10@gmail.com>
2026-08-15 20:40:48 +0000

    Add sample student data
    Inserted initial student data into the Etudiants table.

---
+++ database/data.sql
@@ -0,0 +1,12 @@
+USE gestion_etudiants;
+
+INSERT INTO Etudiants (nom, prenom, age, ville)
+VALUES
+('Diallo', 'Ali', 20, 'Lome'),
+('Ndiaye', 'Sara', 19, 'Lome'),
+('Mahamat', 'Hassan', 22, 'N''Djamena'),
+('Koffi', 'Paul', 18, 'Lome'),
+('Mensah', 'David', 21, 'Accra'),
+('Abdou', 'Amina', 20, 'N''Djamena'),
+('Traore', 'Moussa', 23, 'Bamako'),
+('Kouassi', 'Julie', 19, 'Abidjan');
