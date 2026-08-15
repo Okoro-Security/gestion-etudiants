72e023f46661899c61f698315c982d5b7ada66ea
Lom Emeka Collins <lomemeka10@gmail.com>
2026-08-15 20:46:41 +0000

    Add SQL queries file
    Added SQL queries structure for the gestion_etudiants project.

---
+++ database/queries.sql
@@ -0,0 +1,29 @@
+USE gestion_etudiants;
+
+-- =========================================
+-- REQUÊTES SQL DU PROJET
+-- =========================================
+
+
+-- 1. AFFICHAGE
+
+
+-- 2. FILTRAGE
+
+
+-- 3. MODIFICATION
+
+
+-- 4. SUPPRESSION
+
+
+-- 5. TRI
+
+
+-- 6. AGRÉGATION
+
+
+-- 7. GROUP BY
+
+
+-- 8. HAVING
